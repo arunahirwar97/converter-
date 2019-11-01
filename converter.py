e27256360c8e95127bbf052c3c128aa63277cb31
websiteaap <53229705+websiteaap@users.noreply.github.com>
2019-11-01 17:01:56 +0530

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,9 @@
+print("\t\tConvert the length ")
+print("Milliliters to Liters")
+x = float(input("Enter the value of milliliters : "))
+Li = x/1000
+print("The value of liters :",Li)
+print("Liters to convert into the Milliliters")
+y = float(input("Enter the value of Liters :"))
+Mi = y*1000
+print("Milliliiters value is ",y,"ml Liters values is =",z)
